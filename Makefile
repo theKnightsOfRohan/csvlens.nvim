d7695d38ecb4c37b1b0685a014b5f91df6028cb6
theKnightsOfRohan <shadowsovereign100@gmail.com>
2024-01-12 13:27:40 -0800

    add/Makefile: add makegile for easy formatting and linting

---
+++ Makefile
@@ -0,0 +1,7 @@
+fmt:
+	echo "===> Formatting"
+	stylua lua/ --config-path=.stylua.toml
+
+lint:
+	echo "===> Linting"
+	luacheck lua/ --globals vim
--- lua/csvlens/init.lua
+++ lua/csvlens/init.lua
@@ -1,6 +1,7 @@
-local Terminal = require('toggleterm.terminal').Terminal
+local Terminal = require("toggleterm.terminal").Terminal
 
 ---@class csvlens
+---@field config CsvlensConfig
 ---@field verified boolean
 ---@field setup function
 ---@field open_csv function
@@ -9,69 +10,78 @@ local Terminal = require('toggleterm.terminal').Terminal
 ---@field ends_with function
 local Csvlens = {}
 
+---@class CsvlensConfig
+---@field direction "vertical" | "horizontal" | "tab" | "float",
+Csvlens.config = {
+	direction = "float",
+}
+
 ---@param str string the string we are testing
 ---@param ending string the ending we are testing for
 function Csvlens.ends_with(str, ending)
-    return ending == "" or str:sub(- #ending) == ending
+	return ending == "" or str:sub(-#ending) == ending
 end
 
 ---@param cmd string
 ---@param file string
 function Csvlens.construct_cmd(cmd, file)
-    if not Csvlens.ends_with(file, '.csv') then
-        return nil
-    end
-    return cmd .. ' ' .. file
+	if not Csvlens.ends_with(file, ".csv") then
+		return nil
+	end
+	return cmd .. " " .. file
 end
 
 Csvlens.verified = false
 
+---@param config CsvlensConfig
 ---@return nil
-function Csvlens.setup()
-    Csvlens.verified = Csvlens.check_if_installed()
+function Csvlens.setup(config)
+	Csvlens.config = vim.tbl_deep_extend("force", Csvlens.config, config or {})
 
-    if not Csvlens.verified then
-        local install = vim.fn.input("csvlens not found in PATH. Install automatically? (y/n): ")
+	Csvlens.verified = Csvlens.check_if_installed()
 
-        if (install ~= 'y') then
-            print("csvlens can be installed at https://github.com/YS-L/csvlens")
-            print("If you have already installed csvlens, please add it to your PATH.")
-            return
-        end
+	if not Csvlens.verified then
+		local install = vim.fn.input("csvlens not found in PATH. Install automatically? (y/n): ")
 
-        print("Installing csvlens...")
-        require("csvlens.install"):install_csvlens()
-    end
+		if install ~= "y" then
+			print("csvlens can be installed at https://github.com/YS-L/csvlens")
+			print("If you have already installed csvlens, please add it to your PATH.")
+			return
+		end
 
-    vim.api.nvim_create_user_command('Csvlens', Csvlens.open_csv, {})
+		print("Installing csvlens...")
+		require("csvlens.install"):install_csvlens()
+	end
+
+	vim.api.nvim_create_user_command("Csvlens", Csvlens.open_csv, {})
 end
 
 ---@return nil
 function Csvlens.open_csv()
-    if not Csvlens.verified then
-        print('csvlens tool is not installed or not in your PATH.')
-        print('Install at https://github.com/YS-L/csvlens')
-        return
-    end
+	if not Csvlens.verified then
+		print("csvlens tool is not installed or not in your PATH.")
+		print("Install at https://github.com/YS-L/csvlens")
+		return
+	end
 
-    local file_to_open = vim.fn.expand('%:p')
-    local constructed_cmd = Csvlens.construct_cmd('csvlens', file_to_open)
-    if not constructed_cmd then
-        vim.api.nvim_err_writeln("ERROR: " .. file_to_open .. " is not a csv file.")
-        return
-    end
+	local file_to_open = vim.fn.expand("%:p")
+	local constructed_cmd = Csvlens.construct_cmd("csvlens", file_to_open)
+	if not constructed_cmd then
+		vim.api.nvim_err_writeln("ERROR: " .. file_to_open .. " is not a csv file.")
+		return
+	end
 
-    local csvlens = Terminal:new({
-        cmd = constructed_cmd,
-        close_on_exit = true,
-        direction = 'float',
-    })
-    csvlens:toggle()
+	local csvlens = Terminal:new({
+		cmd = constructed_cmd,
+		close_on_exit = true,
+		direction = Csvlens.config.direction,
+	})
+	csvlens:toggle()
 end
 
 ---@return boolean installed
 function Csvlens.check_if_installed()
-    return vim.fn.executable('csvlens') == 1
+	return vim.fn.executable("csvlens") == 1
 end
 
 return Csvlens
