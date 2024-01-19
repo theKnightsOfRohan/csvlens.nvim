56e92aa5d8cda61deeefb577467f8956cd5de33b
theKnightsOfRohan <shadowsovereign100@gmail.com>
2024-01-19 11:51:34 -0800

    fix/install: refine install instructions and make it synchronous

--- lua/csvlens/install.lua
+++ lua/csvlens/install.lua
@@ -12,7 +12,12 @@ function Installer:install_flow()
 
     if install == "y" then
         print("Installing csvlens...")
-        self:_install_csvlens()
+        local ok, msg = self:_install_csvlens()
+        if not ok then
+            vim.api.nvim_err_writeln(msg)
+        else
+            print(msg)
+        end
     elseif install ~= "n" then
         print("csvlens can be installed at https://github.com/YS-L/csvlens")
         print("If you have already installed csvlens, please add it to your PATH.")
@@ -21,7 +26,7 @@ function Installer:install_flow()
     end
 end
 
-Installer._install_path = vim.env.HOME .. "/.local/bin"
+Installer._install_path = vim.env.HOME .. "/.local/bin/"
 
 ---This function will set the install path for csvlens
 ---CURRENTLY FOR TESTING PURPOSES ONLY
@@ -32,7 +37,9 @@ end
 
 ---This function will construct the url for the csvlens release file
 ---This allows the plugin to be installed on any platform
----@return string
+---@return string url_base
+---@return string filename
+---@return string foldername
 function Installer._construct_release_file_url()
     local os, arch
     local version = "0.6.0"
@@ -40,7 +47,7 @@ function Installer._construct_release_file_url()
     -- check pre-existence of required programs
     if vim.fn.executable("curl") == 0 or vim.fn.executable("tar") == 0 then
         vim.api.nvim_err_writeln("ERROR: both curl and tar are required to install csvlens automatically")
-        return ""
+        return "", "", ""
     end
 
     -- local raw_os = jit.os
@@ -49,8 +56,8 @@ function Installer._construct_release_file_url()
     local os_patterns = {
         ["Windows"] = "pc-windows-msvc",
         ["Windows_NT"] = "pc-windows-msvc",
-        ["Linux"] = "unknown_linux_gnu",
-        ["Darwin"] = "apple_darwin",
+        ["Linux"] = "unknown-linux-gnu",
+        ["Darwin"] = "apple-darwin",
     }
 
     local arch_patterns = {
@@ -63,25 +70,30 @@ function Installer._construct_release_file_url()
 
     if os == nil or arch == nil then
         vim.api.nvim_err_writeln("ERROR: os not supported or could not be parsed")
-        return ""
+        return "", "", ""
     end
 
     -- create the url, filename based on os and arch
-    local filename = "csvlens-" .. arch .. "-" .. os .. (os == "Windows" and ".zip" or ".tar.xz")
-    return "https://github.com/YS-L/csvlens/releases/download/v" .. version .. "/" .. filename
+    local foldername = "csvlens-" .. arch .. "-" .. os
+    local filename = foldername .. (os == "Windows" and ".zip" or ".tar.xz")
+    return "https://github.com/YS-L/csvlens/releases/download/v" .. version .. "/", filename, foldername
 end
 
 ---This function will install the csvlens binary, extract it, and remove the archive
+---@return boolean ok
+---@return string err
 function Installer:_install_csvlens()
-    local release_url = self._construct_release_file_url()
+    local release_url, filename, foldername = self._construct_release_file_url()
     if release_url == "" then
-        return
+        return false, "ERROR: release url could not be constructed"
     end
 
-    local download_command = { "curl", "-sL", "-o", "csvlens.tar.xz", release_url }
-    local extract_command = { "tar", "-zxf", "csvlens.tar.xz", "-C", self._install_path }
-    local output_filename = "csvlens.tar.xz"
-    local binary_path = vim.fn.expand(self._install_path .. "/csvlens")
+    local download_command = { "curl", "-sL", "-o", self._install_path .. filename, release_url .. filename }
+    local extract_command = { "tar", "-xvf", self._install_path .. filename, "-C", self._install_path }
+    local move_command = { "mv", self._install_path .. foldername .. "/csvlens", self._install_path }
+    local cleanup_command = { "rm", "-rf", self._install_path .. foldername, ";", "rm", self._install_path .. filename }
+
+    local binary_path = vim.fn.expand(self._install_path .. "csvlens")
 
     -- check for existing files / folders
     if vim.fn.isdirectory(self._install_path) == 0 then
@@ -92,30 +104,35 @@ function Installer:_install_csvlens()
     if vim.fn.filereadable(binary_path) == 1 then
         local success = vim.loop.fs_unlink(binary_path)
         if not success then
-            vim.api.nvim_err_writeln("ERROR: csvlens binary could not be removed")
-            return
+            return false, "ERROR: old csvlens binary could not be removed"
         end
     end
 
-    -- download and install the csvlens binary
-    local callbacks = {
-        on_sterr = vim.schedule_wrap(function(_, data, _)
-            local out = table.concat(data, "\n")
-            vim.api.nvim_err_writeln(out)
-        end),
-        on_exit = vim.schedule_wrap(function()
-            vim.fn.system(extract_command)
-            -- remove the archive after completion
-            if vim.fn.filereadable(output_filename) == 1 then
-                local success = vim.loop.fs_unlink(output_filename)
-                if not success then
-                    vim.api.nvim_err_writeln("ERROR: existing archive could not be removed")
-                    return
-                end
-            end
-        end),
-    }
-    vim.fn.jobstart(download_command, callbacks)
+    local res = vim.fn.system(download_command)
+
+    if vim.fn.filereadable(self._install_path .. filename) == 0 then
+        return false, "ERROR: csvlens binary could not be downloaded\r\n" .. "ERROR CODE: " .. res
+    end
+
+    res = vim.fn.system(extract_command)
+
+    if vim.fn.isdirectory(self._install_path .. foldername) == 0 then
+        return false, "ERROR: csvlens binary could not be extracted\r\n" .. "ERROR CODE: " .. res
+    end
+
+    res = vim.fn.system(move_command)
+
+    if vim.fn.filereadable(binary_path) == 0 then
+        return false, "ERROR: csvlens binary could not be moved\r\n" .. "ERROR CODE: " .. res
+    end
+
+    res = vim.fn.system(cleanup_command)
+
+    if vim.fn.isdirectory(self._install_path .. foldername) == 1 then
+        return false, "ERROR: csvlens archive could not be removed\r\n" .. "ERROR CODE: " .. res
+    end
+
+    return true, "Csvlens has been installed successfully!"
 end
 
 return Installer
