806eb1f99b2c6076fa0e9639f0dc9d607e0f99bb
theKnightsOfRohan <shadowsovereign100@gmail.com>
2024-01-19 11:52:02 -0800

    add/tests: fix minimal init and add test for installation

--- lua/csvlens/tests/install_spec.lua
+++ lua/csvlens/tests/install_spec.lua
@@ -1,10 +1,24 @@
 local Installer = require("csvlens.install")
 
-local temp_install_path = os.getenv("INSTALLER_DIR") or "/tmp/csvlens_test/installer_test/"
+local temp_install_path = "/tmp/csvlens_test/installer_test/"
 
 describe("Installer", function()
     it(" should be able to set custom install path", function()
         Installer:_set_install_path(temp_install_path)
         assert.are.equal(Installer._install_path, temp_install_path)
+    end)
+
+    it(" should be able to install csvlens", function()
+        Installer:_set_install_path(temp_install_path)
+        assert.are.equal(Installer._install_path, temp_install_path)
+
+        local ok, msg = Installer:_install_csvlens()
+
+        local expected_exec_path = temp_install_path .. "csvlens"
+        local expected_version = "csvlens 0.6.0\n"
+
+        assert.are.equal(ok, true)
+        assert.are.equal(vim.fn.executable(expected_exec_path), 1)
+        assert.are.equal(vim.fn.system(temp_install_path .. "csvlens --version"), expected_version)
     end)
 end)
--- lua/csvlens/tests/minimal_init.lua
+++ lua/csvlens/tests/minimal_init.lua
@@ -1,11 +1,11 @@
-local plenary_dir = os.getenv("PLENARY_DIR") or "/tmp/csvlens_test/plenary.nvim"
+local plenary_dir = "/tmp/csvlens_test/plenary.nvim"
 local is_not_a_directory = vim.fn.isdirectory(plenary_dir) == 0
 if is_not_a_directory then
     print("===> Cloning testing dependency Plenary")
     vim.fn.system({ "git", "clone", "https://github.com/nvim-lua/plenary.nvim", plenary_dir })
 end
 
-local toggleterm_dir = os.getenv("TOGGLETERM_DIR") or "/tmp/csvlens_test/toggleterm.nvim"
+local toggleterm_dir = "/tmp/csvlens_test/toggleterm.nvim"
 is_not_a_directory = vim.fn.isdirectory(toggleterm_dir) == 0
 
 if is_not_a_directory then
